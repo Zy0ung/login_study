c4413b2c08fdbfa3c67d3cba69c34eeb51ea218c
송지영 <jiy0ung_e@sch.ac.kr>
2024-11-25 23:15:12 +0900

    :tada: security jwt 프로젝트 생성

---
+++ security_jwt_login/src/main/java/com/example/security_jwt_login/SecurityJwtLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_jwt_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtLoginApplication.class, args);
+	}
+
+}
