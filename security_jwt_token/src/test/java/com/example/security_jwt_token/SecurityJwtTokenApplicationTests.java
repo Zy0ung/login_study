82f4fbc448655a183dc2593ad20cafcfc935e45b
송지영 <jiy0ung_e@sch.ac.kr>
2024-11-26 23:05:06 +0900

    :tada: spring security + jwt 다중토큰 로그인 프로젝트 시작

---
+++ security_jwt_token/src/test/java/com/example/security_jwt_token/SecurityJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_jwt_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
